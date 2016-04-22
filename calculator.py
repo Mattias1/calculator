9d918024d47d60c9f4fc22f677a19fed7da8e324
Mattias <matheiusb@gmail.com>
2016-04-22 21:55:13 +0200

    Calculate random numbers and strings.

--- calculator.py
+++ calculator.py
@@ -5,19 +5,46 @@
 # A quick commandline calculator.
 #
 import sys
+import random
+import string
 from Tkinter import Tk
 
-# Evaluate the expression
-s = ''.join(sys.argv[1:]) or raw_input('> ')
-if s:
+
+def process(s):
     result = str(eval(s))
-
-    # Print it
     print(result)
+    return result
 
-    # Copy it to clipboard (doesn't work in python 3, so we use python 2)
+
+def copyToClipboard(s):
+    # (Doesn't work in python 3, so we use python 2)
     root = Tk()
     root.withdraw()
     root.clipboard_clear()
-    root.clipboard_append(result)
+    root.clipboard_append(s)
     root.destroy()
+
+
+def randomNumber(n):
+    return ''.join(random.SystemRandom().choice(string.digits) for _ in range(n))
+
+
+def randomString(n):
+    return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(n))
+
+
+def main():
+    s = ''.join(sys.argv[1:]) or raw_input('> ')
+
+    predefinedFunctions = ['randomNumber', 'randomString']
+    for f in predefinedFunctions:
+        if s[:len(f)] == f:
+            n = eval(s[len(f) + 1:])
+            s = '"{}"'.format(globals()[f](n))
+
+    if s:
+        s = process(s)
+        copyToClipboard(s)
+
+
+main()
