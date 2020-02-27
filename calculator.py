b8f3a59706eda96fadc259c54cfe849e032632a1
Mattias <mattias.beimers@topicus.nl>
2020-02-27 16:16:41 +0100

    Add random uuid function

--- calculator.py
+++ calculator.py
@@ -7,14 +7,13 @@
 import sys
 import random
 import string
+import uuid
 from Tkinter import Tk
-
 
 def process(s):
     result = str(eval(s))
     print(result)
     return result
-
 
 def copyToClipboard(s):
     # (Doesn't work in python 3, so we use python 2)
@@ -24,21 +23,21 @@ def copyToClipboard(s):
     root.clipboard_append(s)
     root.destroy()
 
-
 def randomNumber(n):
     return ''.join(random.SystemRandom().choice(string.digits) for _ in range(n))
-
 
 def randomString(n):
     return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(n))
 
+def randomUuid(n):
+    return str(uuid.uuid4())
 
 def main():
     s = ''.join(sys.argv[1:]) or raw_input('> ')
     if not s:
         return
 
-    predefinedFunctions = ['randomNumber', 'randomString']
+    predefinedFunctions = ['randomNumber', 'randomString', 'randomUuid']
     for f in predefinedFunctions:
         if s[:len(f)] == f:
             n = eval(s[len(f) + 1:])
@@ -46,6 +45,5 @@ def main():
 
     s = process(s)
     copyToClipboard(s)
-
 
 main()
